dfd2c0f3ba1c2b69d1fd18a27b282868022efe42
Caio Ferrarezi <caioferrarezi2@gmail.com>
2021-05-02 02:48:30 -0300

    :construction: wip(default) add blog post list feature and darkmode support

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import theme from '../lib/theme'
+
+import 'normalize.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  theme.update()
+
   return <Component {...pageProps} />
 }
 
